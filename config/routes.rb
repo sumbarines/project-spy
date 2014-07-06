6e073a54bd4238899688c898d9c2c17329057ab8
sumbarines <sumbev@gmail.com>
2014-07-06 04:42:32 +0000

    Experimenting with routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ TrackApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+   root 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
